87501d51be7df9d724a56b2278396ed5c1582ced
Gus Waldspurger <gus@waldspurger.com>
2023-06-20 10:24:44 -0700

    add uthash

--- minio/miniomodule.c
+++ minio/miniomodule.c
@@ -1,7 +1,7 @@
 /* Author:  Gus Waldspurger
    Date:    June 16, 2023.
 
-   Implementation of a MinIO cache.
+   Implementation of a MinIO file cache.
    */
 
 #include "miniomodule.h"
@@ -10,7 +10,9 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <sys/mman.h>
 
+#include <include/uthash.h>
 
 /* ------------------------ */
 /*   REPLACEMENT POLICIES   */
@@ -29,6 +31,8 @@ policy_FIFO(cache_t *cache, void *item, size_t size)
 static uint64_t
 policy_MINIO(cache_t *cache, void *item, size_t size)
 {
+   static uint8_t *next;
+
    return NULL;
 }
 
@@ -43,24 +47,40 @@ static policy_func_t *policy_table[N_POLICIES] = {
 /*   INTERFACE   */
 /* ------------- */
 
-/* Problems/TODO
-
-   Not all images are going to be the same size, so how do we decide which ones
-   to cache without already knowing all of their sizes?
-
-*/
-
-
-/* Read an item from the cache CACHE into DATA. */
-bool
-cache_get(cache_t *cache, void *data)
+/* Read an item from CACHE into DATA, indexed by FILEPATH, and located on the
+   filesystem at FILEPATH. If the cached file is greater than MAX_SIZE bytes,
+   DATA is filled with the first MAX_SIZE bytes, and -1 is returned. Otherwise,
+   0 is returned on success. */
+int
+cache_get(cache_t *cache, char *filepath, void *data, uint64_t max_size)
 {
+   /* TODO: use direct IO to read in the file, to bypass the page cache. */
    return NULL;
 }
 
-/* Initialize a cache CACHE with SIZE bytes and POLICY replacement policy. */
-bool
+/* Initialize a cache CACHE with SIZE bytes and POLICY replacement policy. On
+   success, 0 is returned. On failure, -1 is returned. */
+int
 cache_init(cache_t *cache, size_t size, policy_t policy)
 {
-   return NULL;
+   /* Cache configuration. */
+   cache->size = size;
+   cache->used = 0;
+   cache->policy = policy;
+
+   /* TODO: initialize the filename -> void * hash table. */
+
+   /* Allocate the cache's memory. */
+   if ((cache->data = malloc(size)) == NULL) {
+      fprintf(stderr, "Failed to allocate the cache's memory. Aborting.\n");
+      return -1;
+   }
+
+   /* Pin the cache's memory. */
+   if (mlock(cache->data, size) != 0) {
+      fprintf(stderr, "Failed to pin the cache's memory. Aborting.\n");
+      return -1;
+   }
+
+   return 0;
 }
--- minio/miniomodule.h
+++ minio/miniomodule.h
@@ -1,7 +1,7 @@
 /* Author:  Gus Waldspurger
    Date:    June 16, 2023.
 
-   Implementation of a MinIO cache.
+   Implementation of a MinIO file cache.
    */
 
 #ifndef __MINIO_MODULE_H_
@@ -16,11 +16,20 @@ typedef enum {
     N_POLICIES
 } policy_t;
 
+/* TODO find a hash table library to use. */
+typedef int hash_t;
+
 /* Cache. */
 typedef struct {
+    /* Configuration. */
     policy_t policy;    /* Replacement policy. Only MinIO supported. */
-    size_t size;        /* Size of cache in bytes. */
-    size_t used;        /* Number of bytes cached. */
+    size_t   size;      /* Size of cache in bytes. */
+    size_t   used;      /* Number of bytes cached. */
+
+    /* State. */
+    uint8_t *data;      /* SIZE bytes of memory. */
+    uint8_t *next;      /* Next unused byte of memory. */
+    hash_t  *ht;        /* Hash table, maps filename to beginning of data. */
 } cache_t;
 
 #endif
